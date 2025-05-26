f5b6e334d32f701384fdd4d3c0b8605f7b813d95
Boris Kalapchiev <boriskalapchiev@gmail.com>
2025-05-26 15:40:35 +0300

    Task 3

---
+++ 03-01.ts
@@ -0,0 +1,17 @@
+// Write a function that takes in a tuple with exactly two elements:
+// 1. The first parameter is a string and represents the person’s name.
+// 2. The second parameter is a number and represents the person’s age.
+// In case a correct tuple is passed, your function should return a string with the following format:
+// 'Hello, my name is <name> and my age is <age>'. In case an invalid tuple is passed, TS should mark it as an error.
+
+function formatPerson(personInfo: [name: string, age: number]): string {
+    const [name, age] = personInfo;
+
+    const result = `Hello, my name is ${name} and my age is ${age}`;
+
+    return result;
+}
+
+console.log(formatPerson(['Ivan', 20]))
+console.log(formatPerson(['Joro', 30]))
+console.log(formatPerson(['Joro', '25']))
